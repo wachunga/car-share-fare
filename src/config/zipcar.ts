ec144ddd99f10dfea8a9ee431b393e9759f375dd
David Hirtle <wachunga@gmail.com>
2019-07-07 18:23:02 -0700

    add stub for zipcar

---
+++ src/config/zipcar.ts
@@ -0,0 +1,28 @@
+// import { PackageConfig, CarShareConfig } from 'config';
+
+// varies per vehicle and city - eg Vancouver $10/hour, $16.50, $20 etc
+// website shows conflicting prices... is map the most correct?
+// 200 km included per 24 hour period, 0.30 per km after that
+// no per-trip fee mentioned
+
+// const commonConfig: CarShareConfig = {
+//   service: 'Zipcar',
+//   url: 'https://www.zipcar.com/en-ca/vancouver',
+//   lastUpdated: '2019-07-02',
+//   currency: 'CAD',
+//   fees: {
+//     monthly: 7,
+//     registration: 0,
+//   },
+// };
+
+// const packages: PackageConfig[] = [
+//   {
+//     name: 'Economy',
+//     ...commonConfig,
+//     vehicle: 'Elantra, Jetta, Sentra',
+//     maxPassengers: 5,
+//     time: [{ per: 60, cost: 10 }, { per: 60 * 24, cost: 79.5 }],
+//     distance: common200KmIncluded,
+//   },
+// ];
