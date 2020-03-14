f49e8e12c3596b71128868575aff1f38faa9a24b
David Hirtle <wachunga@gmail.com>
2020-03-13 21:12:00 -0700

    Fix build - do not serve except on start
    and upgrade to latest tsdx to get support for copy

--- tsdx.config.js
+++ tsdx.config.js
@@ -1,25 +1,14 @@
-const serve = require('rollup-plugin-serve');
-// const copy = require('rollup-plugin-copy');
+const copy = require('rollup-plugin-copy');
 
 module.exports = {
   rollup(config, options) {
-    if (options.writeMeta) {
-      // only add the first time to avoid EADDRINUSE
-      config.plugins.push(
-        serve({
-          open: true,
-          contentBase: 'dist',
-        })
-        // TODO: add copy of index etc (blocked by https://github.com/jaredpalmer/tsdx/pull/208)
-        // copy({
-        //   targets: [
-        //     { src: 'www/**/*', dest: 'dist' },
-        //   ],
-        //   verbose: true,
-        //   copyOnce: true,
-        // })
-      );
-    }
+    config.plugins.push(
+      copy({
+        targets: [{ src: 'www/**/*', dest: 'dist' }],
+        verbose: true,
+        copyOnce: true,
+      })
+    );
     return config;
   },
 };
